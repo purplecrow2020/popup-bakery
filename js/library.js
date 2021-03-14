c2799fbadd623206c25dc17f11249d22c297a684
purplecrow2020 <akshat@doubtnut.com>
2021-03-14 20:20:06 +0530

    align flex dependency removal

--- js/library.js
+++ js/library.js
@@ -4,15 +4,14 @@ const popupId = popup.getAttribute('popup-code');
 
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
+  console.log('oko ');
   if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
     let popup_html = JSON.parse(this.responseText)['popup_html'];
     setTimeout(function(){
         document.getElementById("popup-view").innerHTML = popup_html;
     },1000);
   }
 };
-xhttp.open("GET", `http://localhost:8000/api/get-html/${popupId}`, true);
+xhttp.open("GET", `http://128.199.19.190:8001/api/get-html/${popupId}`, true);
 xhttp.send();
-
-
-
